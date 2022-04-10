745c74f0886c9c5505b05ef8bfc963c329574dfb
Alexandre Adonai Gama da Silva <63507690+Alexandreaags@users.noreply.github.com>
2022-04-10 17:33:22 -0300

    Questão 5 concluída

---
+++ q5.py
@@ -0,0 +1,11 @@
+import cmath
+
+Ia1 = complex(17.321, 10)/complex(7.102,14.408)
+Ia2 = Ia1*5/7
+Ia0 = Ia1 - Ia2
+p = abs(Ia2)**2 * 10
+
+print(f'A corrente I1 é igual a {Ia1:.2f} A')
+print(f'A corrente I2 é igual a {Ia2:.2f} A')
+print(f'A corrente I0 é igual a {Ia0:.2f} A')
+print(f'A potência média liberada é igual a {p:.2f} W')
